9f5cdc3da76a31f11881382f615270e00e6822d4
vercyngetoryks <michal.krakowinski.gmail.com>
2024-12-13 13:29:11 +0100

    add function to display books from myLibrary

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 "use strict";
 
 const myLibrary = [];
+const library = document.querySelector(".library");
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read === true ? "read already" : "not read yet";
+    this.read = read === true ? "I read it already" : "Not read it yet";
   }
 }
-
-// Book.prototype.info = function () {
-//   return `${this.title} by ${this.author}, ${this.pages} pages, ${
-//     this.read === "yes" ? "read already" : "not read yet"
-//   }`;
-// };
-
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
-
-// console.log(theHobbit.info());
 
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
@@ -29,3 +20,17 @@ addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
 addBookToLibrary("1984", "George Orwell", 328, true);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
 addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+      <h2>${book.title}</h2>
+      <p>by</p>
+      <h3>${book.author}</h3>
+      <p>${book.pages} pages</p>
+      <p>${book.read}</p>`;
+    library.appendChild(card);
+  });
+}

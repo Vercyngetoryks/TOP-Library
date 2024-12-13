fc791be97d08a3f879d47dea1605d6fd11baebc1
vercyngetoryks <michal.krakowinski.gmail.com>
2024-12-13 12:11:04 +0100

    add some books to myLibrary

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 "use strict";
 
+const myLibrary = [];
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read === true ? "read already" : "not read yet";
   }
 }
 
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${
-    this.read === "yes" ? "read already" : "not read yet"
-  }`;
-};
+// Book.prototype.info = function () {
+//   return `${this.title} by ${this.author}, ${this.pages} pages, ${
+//     this.read === "yes" ? "read already" : "not read yet"
+//   }`;
+// };
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
 
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+  return;
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);

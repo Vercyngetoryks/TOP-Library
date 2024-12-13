554de55b2de6ba7a7708916f637159a5e668e212
vercyngetoryks <michal.krakowinski.gmail.com>
2024-12-13 09:53:42 +0100

    Write a constructor for making Book objects

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+"use strict";
 
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${
+    this.read === "yes" ? "read already" : "not read yet"
+  }`;
+};
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "no");
+
+console.log(theHobbit.info());

afc9a72a74c6f4f22c7a8a0297bfebfa877c8420
vercyngetoryks <michal.krakowinski.gmail.com>
2024-12-15 19:23:39 +0100

    implement add new book feature

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const library = document.querySelector(".library");
 const showButton = document.getElementById("showDialog");
 const cancelButton = document.getElementById("cancelBtn");
 const dialogBox = document.querySelector(".dialog");
+const form = document.querySelector(".bookInputs");
 const bookTitleInput = document.getElementById("bookTitle");
 class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read === true ? "I read it already" : "Not read it yet";
+    this.read = read === "yes" ? "I read it already" : "Not read it yet";
   }
 }
 
@@ -20,16 +21,17 @@ function addBookToLibrary(title, author, pages, read) {
   return;
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-addBookToLibrary("1984", "George Orwell", 328, true);
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+function initializeLibrary() {
+  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+  addBookToLibrary("1984", "George Orwell", 328, true);
+  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
+  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+}
 
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = `
+function createCard(book) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
       <h2 class="header-secondary">${book.title}</h2>
       <p>by</p>
       <h3 class="header-tertiary">${book.author}</h3>
@@ -37,11 +39,35 @@ function displayBooks() {
       <p>${book.pages} pages</p>
       <br>
       <p>${book.read}</p>`;
-    library.appendChild(card);
+  library.appendChild(card);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    createCard(book);
   });
 }
 
+function renderLastBook() {
+  const book = myLibrary[myLibrary.length - 1];
+  createCard(book);
+}
+
+initializeLibrary();
 displayBooks();
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
+  const pages = document.getElementById("bookPages").value;
+  const read = document.getElementById("bookRead").value;
+
+  addBookToLibrary(title, author, pages, read);
+  renderLastBook();
+  dialogBox.close();
+  form.reset();
+});
 
 showButton.addEventListener("click", () => {
   dialogBox.showModal();

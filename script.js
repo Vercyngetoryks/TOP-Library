50ee39a0a70f62e84d343e30234ac966a91de7ee
vercyngetoryks <michal.krakowinski.gmail.com>
2024-12-24 08:54:48 +0100

    add toggle read status feature

--- script.js
+++ script.js
@@ -16,16 +16,22 @@ class Book {
   }
 }
 
+Book.prototype.toggleRead = function () {
+  this.read === "I read it already"
+    ? (this.read = "Not read it yet")
+    : (this.read = "I read it already");
+};
+
 function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
   return;
 }
 
 function initializeLibrary() {
-  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
-  addBookToLibrary("1984", "George Orwell", 328, true);
-  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, false);
-  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+  addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "yes");
+  addBookToLibrary("1984", "George Orwell", 328, "yes");
+  addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "no");
+  addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, "no");
 }
 
 function createCard(book) {
@@ -42,7 +48,17 @@ function createCard(book) {
       <br>
       <p>${book.read}</p>
     </div>
-    <button class="btnDel">Delete</button>`;
+    <div class="cardBtns">
+    <button class="btnRead"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btnCard-icon">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+</svg>
+</buttton>
+    <button class="btnDel"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="btnCard-icon">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+</button>
+    </div>
+    `;
   library.appendChild(card);
 }
 
@@ -82,6 +98,16 @@ library.addEventListener("click", (e) => {
     document.querySelectorAll(".card").forEach((card, index) => {
       card.dataset.bookId = index;
     });
+  }
+});
+
+library.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btnRead")) {
+    const card = e.target.closest(".card");
+    const bookId = card.dataset.bookId;
+    const book = myLibrary[bookId];
+    book.toggleRead();
+    card.querySelector("p:last-of-type").textContent = book.read;
   }
 });
 
